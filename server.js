870155919656b42679c5944d3173dbf3fa437868
alexandrabaluta <alexandrabaluta99@yahoo.ro>
2023-01-09 13:09:35 +0200

    added server.js for deployment on Heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let app = express();
+app.use(express.static(__dirname+'/dist/proiectawdfinal'));
+app.get('/*', (req, resp)=>
+{
+  resp.sendFile(__dirname+'/dist/proiectawdfinal/index.html');
+})
+
+app.listen(process.env.PORT || 8080);
